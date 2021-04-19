7ad2f7659d97c49228ab33c99e3caa2e89a4a83b
Ivan Karachentsev <karachencew@gmail.com>
2021-04-19 18:41:56 +0300

    fix build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import compress from 'vite-plugin-compress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), compress(),]
+  plugins: [reactRefresh()]
 })
